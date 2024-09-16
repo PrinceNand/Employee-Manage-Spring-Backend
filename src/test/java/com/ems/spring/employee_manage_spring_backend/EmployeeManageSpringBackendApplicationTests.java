8ddcd8775b4eea326ca275bcbfb6a6bd559eddb7
Ashley <ashnand2000@gmail.com>
2024-09-16 18:20:06 +0530

    Initial Setup

---
+++ src/test/java/com/ems/spring/employee_manage_spring_backend/EmployeeManageSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.spring.employee_manage_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManageSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
