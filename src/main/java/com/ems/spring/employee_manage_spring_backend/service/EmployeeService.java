61549a718b520f7a2b2b031bc58a2b131b1ca0c0
Prince <ashnand2000@gmail.com>
2024-09-17 14:59:11 +0530

    EMS: 6.(Service) EmployeeService is created to connect with controller and send data

---
+++ src/main/java/com/ems/spring/employee_manage_spring_backend/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.ems.spring.employee_manage_spring_backend.service;
+
+import com.ems.spring.employee_manage_spring_backend.dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+    EmployeeDTO createEmployee(EmployeeDTO employeeDTO);
+}
